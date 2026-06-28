926faef7d352f8587499a159e4113335a66a8694
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-06-28 22:28:50 +0530

    [Gayatri:Welcome to Address Book Problem]

---
+++ AdressBookProblem.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz.AddressBook;
+
+class AddressBookProblem {
+    public static void main(String[] args) {
+        System.out.print("Welcome to Address Book Problem");
+    }
+}
